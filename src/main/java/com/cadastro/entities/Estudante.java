50dd1cbb0ca4d8c65f0cae8bcaa8e31d6c18ef69
ThiagoHBarros <thiagohbarros1@hotmail.com>
2021-06-24 14:41:57 -0300

    Add Entity and Repository

---
+++ src/main/java/com/cadastro/entities/Estudante.java
@@ -0,0 +1,81 @@
+package com.cadastro.entities;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "TB_ESTUDANTE")
+public class Estudante implements Serializable{
+
+
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;  
+
+    private String name;
+    private String email;
+    private String tel;
+    private String endereco;
+
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getTel() {
+        return tel;
+    }
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+    public String getEndereco() {
+        return endereco;
+    }
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + (int) (id ^ (id >>> 32));
+        return result;
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Estudante other = (Estudante) obj;
+        if (id != other.id)
+            return false;
+        return true;
+    }
+
+    
+    
+}
